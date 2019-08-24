8e6c3212b3116c1133c7104c513b8ef4bbeddca9
Allistair <you@example.com>
2019-08-24 18:01:47 -0500

    Setup MobxRouter

--- app/index.js
+++ app/index.js
@@ -1,121 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Card } from 'antd';
+import { MobxRouter, RouterStore, startRouter } from 'mobx-router';
+import { Provider } from 'mobx-react';
 import 'antd/dist/antd.css';
 import './index.css';
-import Layout from './Components/Layout';
+import SessionStore from './stores/SessionStore';
+import views from './config/views';
 
 
 
 
-const tabList = [
-    {
-        key: 'tab1',
-        tab: 'tab1'
-    },
-    {
-        key: 'tab2',
-        tab: 'tab2'
-    }
-];
-
-
-const contentList = {
-    tab1: <p>content1</p>,
-    tab2: <p>content2</p>
+const store = {
+	SessionStore: new SessionStore(),
+	router: new RouterStore()
 };
 
-
-const tabListNoTitle = [
-    {
-        key: 'article',
-        tab: 'article'
-    },
-    {
-        key: 'app',
-        tab: 'app'
-    },
-    {
-        key: 'project',
-        tab: 'project'
-    }
-];
+startRouter(views, store);
 
 
-const contentListNoTitle = {
-    article: <p>article content</p>,
-    app: <p>app content</p>,
-    project: <p>project content</p>
-};
+const Root = (
+    <Provider store={store}>
+        <MobxRouter />
+    </Provider>
+)
 
 
-class App extends Component {
-    state = {
-        key: 'tab1',
-        noTitleKey: 'app',
-        records: []
-    };
-
-
-    onTabChange = (key, type) => {
-        console.log(key, type);
-        this.setState({ [type]: key });
-    };
-
-
-    getRecords = async () => {
-        await fetch('/xhr/records')
-            .then(response => {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' + response.status);
-                    return;
-                }
-
-                response.json().then(data => {
-                    this.setState({ records: data });
-                });
-            })
-            .catch(err => {
-                console.log('Fetch Error :-S', err);
-            });
-    };
-
-    render() {
-        const { records } = this.state;
-        console.log('TCL: App -> render -> records', records);
-
-        return (
-            <Layout>
-                <div className="App">
-                    <h1>Welcome to the test store</h1>
-                    <div style={{ marginTop: '16px' }}>
-                        <Button type="primary" onClick={this.getRecords}>
-                            Get Records
-                        </Button>
-                        <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gridGap: '1rem' }}>
-                            {records.map(item => {
-                                return (
-                                    <Card
-                                        style={{ width: '300px' }}
-                                        cover={<img alt="example" src="https://source.unsplash.com/1600x900/?clothes" />}
-                                        title={item.name}
-                                        extra={<a href="#">More</a>}
-                                        tabList={tabList}
-                                        activeTabKey={this.state.key}
-                                        onTabChange={key => {
-                                            this.onTabChange(key, 'key');
-                                        }}
-                                    >
-                                        {contentList[this.state.key]}
-                                    </Card>
-                                );
-                            })}
-                        </div>
-                    </div>
-                </div>
-            </Layout>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(Root, document.getElementById('app'));
