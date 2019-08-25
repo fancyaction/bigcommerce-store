4c065cc695ccbaaa3dd5b217d35f5d495cdbeb85
fancyaction <labcoatdisaster@gmail.com>
2019-08-25 14:58:18 -0500

    Add select input helper componets for form

---
+++ app/Components/Create/formHelpers.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Select } from 'antd';
+
+
+
+
+const { Option } = Select;
+
+
+const CategorySelect = ({ options, handleSelectChange}) => (
+    <Select
+        mode="multiple"
+        style={{ width: 200 }}
+        placeholder="Select category"
+        onChange={handleSelectChange}
+    >
+        {options.map((item, i) => <Option value={item.id} key={`${item.name}-[${i}]`}>{item.name}</Option>)}
+    </Select>
+);
+
+
+const ConditionSelect = ({ handleSelectChange }) => (
+    <Select
+        style={{ width: 200 }}
+        placeholder="Select condition"
+        onChange={handleSelectChange}
+    >
+        <Option value="Used,">Used</Option>
+        <Option value="New">New</Option>
+        <Option value="Refurbished">Refurbished</Option>
+    </Select>
+);
+
+
+const TypeSelect = ({ handleSelectChange }) => (
+    <Select
+        style={{ width: 200 }}
+        placeholder="Select condition"
+        onChange={handleSelectChange}
+    >
+        <Option value="physical">physical</Option>
+        <Option value="digital">digital</Option>
+    </Select>
+);
+
+
+export {
+    CategorySelect,
+    ConditionSelect,
+    TypeSelect
+}
