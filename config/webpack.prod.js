c4792c6589d294f6db2e127f4bc346af9ea18909
fancyaction <labcoatdisaster@gmail.com>
2019-08-25 18:35:44 -0500

    revert build

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,6 @@
-const path =  require('path');
-const HtmlWebpackPlugin =  require('html-webpack-plugin');
-const webpack =  require('webpack');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
 
 
