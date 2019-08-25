4b25605ca0b6dc418aa3282deff0ba43faf12857
fancyaction <labcoatdisaster@gmail.com>
2019-08-25 14:57:32 -0500

    Add bodyparser for post calls

--- server/app.js
+++ server/app.js
@@ -2,11 +2,17 @@ const path = require('path');
 const express = require("express");
 let app = express();
 const routes = require('./routes');
+const bodyParser = require('body-parser');
+
 
 
 
 
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(bodyParser.json({limit: '128mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '128mb' }));
+
 app.use('/', routes);
 
 module.exports = app;
