c86ded4425b96046e8acf2c661825a045d43a912
fancyaction <labcoatdisaster@gmail.com>
2019-08-25 20:36:58 -0500

    update build for heroku

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,6 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 
 
